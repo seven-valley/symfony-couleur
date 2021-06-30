c63f10ffb2c30c0bafdd6f2a7f5d2a4ea8aa44a7
Seven Valley <augure@gmail.com>
2021-06-30 10:00:26 +0200

    C CREATE ajouter une couleur

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,7 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Couleur;
 use App\Repository\CouleurRepository;
-use Doctrine\ORM\EntityManagerInterface; 
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\CouleurType;
 use DateTime;
 class MainController extends AbstractController
 {
@@ -58,6 +60,56 @@ class MainController extends AbstractController
         $em->flush(); // COMMIT
         return $this->redirectToRoute('route_home'); 
         
+    }
+
+    /**
+     * @Route("/add-brut", name="couleur_add_brut")
+     */
+    public function add_brut(EntityManagerInterface $em ,Request $request): Response
+    {
+        $couleur = $request->request->get('couleur');
+        // dd vardump and die 
+        // dump() 
+        //dd($c);
+       
+        $c = new Couleur();
+        $c->setTitle($couleur);
+        $c->setTendance(true);
+        $em->persist($c);
+        $em->flush(); // COMMIT
+        return $this->redirectToRoute('route_home'); 
+               
+    }
+
+    /**
+     * @Route("/add-normal", name="couleur_add_normal")
+     */
+    public function add_normal(EntityManagerInterface $em ,Request $request): Response
+    {
+       
+        $c = new Couleur(); // je crée un Entity
+        // je viens créer un formulaire
+        // et associer l'obejet $c au formulaire
+        $form = $this->createForm(CouleurType::class, $c);
+        
+         // traiter le formulaire
+         // hydrater $c : $c->setTitle($couleur);
+         $form->handleRequest($request);
+         
+         if($form->isSubmitted()) // si formulaire submit
+         {
+            // tendance = true
+            $c->setTendance(true); 
+            $em->persist($c);
+            $em->flush(); // COMMIT
+            return $this->redirectToRoute('route_home'); 
+         }
+
+        
+        return $this->render('main/ajouter.html.twig', [
+           'couleurForm' => $form->createView(),
+        ]);
+               
     }
    
 }
